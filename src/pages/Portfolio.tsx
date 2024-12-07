68592f5a4a678b1cc8e04e33c9300ea35ce5ac4e
mblmarlon98 <berdefymarlon@gmail.com>
2024-12-07 14:37:38 +0800

    Initial commit

---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface PortfolioProps {
+}
+
+export default class Portfolio extends React.Component<PortfolioProps> {
+  public render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
